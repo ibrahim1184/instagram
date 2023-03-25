5014c45881ae73119a256ea4f6c78684c1ec3e2e
ibrahim1184 <ibrahim.buu@gmail.com>
2023-03-25 21:24:29 +0300

    veriler silindi

--- lib/core/data/stories.dart
+++ lib/core/data/stories.dart
@@ -1,35 +1,3 @@
 import 'package:initial/core/utils/assets.dart';
 
-const testStories = [
-  // Çok veri
-  {"name": "Can Kurt", "profileImg": ImageRes.p1},
-  {"name": "İbrahim Uğur", "profileImg": ImageRes.p2},
-  {"name": "Mehmet Bal", "profileImg": ImageRes.p3},
-  {"name": "Can Kurt", "profileImg": ImageRes.p1},
-  {"name": "İbrahim Uğur", "profileImg": ImageRes.p2},
-  {"name": "Mehmet Bal", "profileImg": ImageRes.p3},
-  {"name": "Ali Dolgun", "profileImg": ImageRes.p4},
-  {"name": "Ufuk Akkuş", "profileImg": ImageRes.p5},
-  {"name": "Ali Dolgun", "profileImg": ImageRes.p4},
-  {"name": "Ufuk Akkuş", "profileImg": ImageRes.p5},
-  {"name": "Can Kurt", "profileImg": ImageRes.p1},
-  {"name": "İbrahim Uğur", "profileImg": ImageRes.p2},
-  {"name": "Mehmet Bal", "profileImg": ImageRes.p3},
-  {"name": "Can Kurt", "profileImg": ImageRes.p1},
-  {"name": "İbrahim Uğur", "profileImg": ImageRes.p2},
-  {"name": "Mehmet Bal", "profileImg": ImageRes.p3},
-  {"name": "Ali Dolgun", "profileImg": ImageRes.p4},
-  {"name": "Ufuk Akkuş", "profileImg": ImageRes.p5},
-  {"name": "Ali Dolgun", "profileImg": ImageRes.p4},
-  {"name": "Ufuk Akkuş", "profileImg": ImageRes.p5},
-  {"name": "Can Kurt", "profileImg": ImageRes.p1},
-  {"name": "İbrahim Uğur", "profileImg": ImageRes.p2},
-  {"name": "Mehmet Bal", "profileImg": ImageRes.p3},
-  {"name": "Can Kurt", "profileImg": ImageRes.p1},
-  {"name": "İbrahim Uğur", "profileImg": ImageRes.p2},
-  {"name": "Mehmet Bal", "profileImg": ImageRes.p3},
-  {"name": "Ali Dolgun", "profileImg": ImageRes.p4},
-  {"name": "Ufuk Akkuş", "profileImg": ImageRes.p5},
-  {"name": "Ali Dolgun", "profileImg": ImageRes.p4},
-  {"name": "Ufuk Akkuş", "profileImg": ImageRes.p5}
-];
+const testStories = [];
